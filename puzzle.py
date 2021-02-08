3e71500bfae05f678878daaa1cd2f9461a4b33be
bohdaholas <bohdan.ivashko@ucu.edu.ua>
2021-02-09 00:10:19 +0200

    Added puzzle.py, check_row function and extra is_number function

---
+++ puzzle.py
@@ -0,0 +1,44 @@
+"""
+Module that deals with puzzle
+Github repository: https://github.com/bohdaholas/puzzle.git
+"""
+
+board_example = [
+ "**** ****",
+ "***1 ****",
+ "**  3****",
+ "* 4 1****",
+ "     9 5 ",
+ " 6  83  *",
+ "3   1  **",
+ "  8  2***",
+ "  2  ****"
+]
+
+
+def is_number(value):
+    """
+    Determine whether a value is a number or not
+    >>> is_number(5)
+    True
+    >>> is_number("1")
+    True
+    """
+    try:
+        int(value)
+        return True
+    except ValueError:
+        return False
+
+
+def check_row(board):
+    """
+    Check row according to rules
+    >>> check_row(board_example)
+    True
+    """
+    for row in board:
+        values = [x for x in row if is_number(x)]
+        if len(values) != len(set(values)):
+            return False
+    return True
